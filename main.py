d6f9d7c8e04bc50b82320093d8212a0ab3577b31
harryo583 <harrywang@u.northwestern.edu>
2024-11-21 12:07:24 -0600

    initial framework established

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys 
+import json 
+
+def main(input_file, output_file):
+    
+    
+    
+    return
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Invalid argument count")
+        sys.exit(1)
+    main(sys.argv[1],sys.argv[2])
